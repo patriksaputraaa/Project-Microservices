2325d1d6be1ba5fb887a42bf2e1266c0a73d8d96
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2025-02-27 23:28:58 +0700

    Update Dtos.cs

--- Play.Catalog.Service/Dtos/Dtos.cs
+++ Play.Catalog.Service/Dtos/Dtos.cs
@@ -5,5 +5,7 @@ using System.Threading.Tasks;
 
 namespace Play.Catalog.Service.Dtos
 {
-    
+    public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
+    public record CreateItemDto(string Name, string Description, decimal Price);
+    public record UpdateItemDto(string Name, string Description, decimal Price);
 }
