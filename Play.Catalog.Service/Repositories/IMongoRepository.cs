f646f3d89967613b48dc3602b017dff4a0542c06
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2025-03-21 23:13:54 +0700

    Update IMongoRepo

--- Play.Catalog.Service/Repositories/IMongoRepository.cs
+++ Play.Catalog.Service/Repositories/IMongoRepository.cs
@@ -2,11 +2,55 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Play.Catalog.Service.Entities;
+using MongoDB.Driver;
 
 namespace Play.Catalog.Service.Repositories
 {
-    public class MongoRepository : IRepository<T> where T : IEntity
+    public class MongoRepository<T> : IRepository<T> where T : IEntity
     {
-        
+        private readonly IMongoCollection<T> dbCollection;
+        private readonly FilterDefinitionBuilder<T> filterBuilder = Builders<T>.Filter;
+
+        public MongoRepository(IMongoDatabase database, string collectionName)
+        {
+            dbCollection = database.GetCollection<T>(collectionName);
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            if(entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            await dbCollection.InsertOneAsync(entity);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            FilterDefinition<T> filter = filterBuilder.Eq(T => T.Id, id);
+            await dbCollection.DeleteOneAsync(filter);
+        }
+
+        public async Task<IReadOnlyCollection<T>> GetAllAsync()
+        {
+            return await dbCollection.Find(filterBuilder.Empty).ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            FilterDefinition<T> filter = filterBuilder.Eq(T => T.Id, id);
+            return await dbCollection.Find(filter).SingleOrDefaultAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            if (entity is null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            FilterDefinition<T> filter = filterBuilder.Eq(existingT => existingT.Id, entity.Id);
+            await dbCollection.ReplaceOneAsync(filter, entity);
+        }
     }
 }
--- Play.Catalog.Service/obj/Debug/net9.0/Play.Catalog.Service.AssemblyInfo.cs
+++ Play.Catalog.Service/obj/Debug/net9.0/Play.Catalog.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7d176c29d917396d62d197c09dac56d363bd01e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38a4abddae1efdca9848264d69c968bf84238cb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
