858a862d5b55b801b2b33f9ded835e47db4cbf3b
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2025-03-22 08:37:34 +0700

    Dependency Injection

--- MicroserviceProject/Play.Common/src/Play.Common.Service/obj/Debug/net9.0/Play.Common.Service.AssemblyInfo.cs
+++ MicroserviceProject/Play.Common/src/Play.Common.Service/obj/Debug/net9.0/Play.Common.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Play.Common.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fda169c516597b5dae1b4b7cbfb99d17f6e09ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+437dfd9aa149fc4474efb8164319e6996ac56d14")]
 [assembly: System.Reflection.AssemblyProductAttribute("Play.Common.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Play.Common.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
